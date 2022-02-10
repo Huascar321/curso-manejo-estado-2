d09dd7344b93126d2a93389ddb2f4594f0b104a3
Huascar <oxhuascar321@gmail.com>
2022-02-09 23:28:27 -0400

    Composicion de componentes y colocacion del estado

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,14 +1,61 @@
 import React from "react";
-import { TodoProvider } from "../../TodoContext";
-import { AppUI } from "./AppUI";
+/* import { TodoProvider } from "../../TodoContext";
+import { AppUI } from "./AppUI"; */
+
+const tempStyle = {
+  color: 'black'
+};
 
 function App() {
+  const [state, setState] = React.useState('estado compartido');
+
+  return (
+    <>
+      <TodoHeader>
+        <TodoCounter />
+        <TodoSearch />
+      </TodoHeader>
+      <TodoList>
+        <TodoItem state={state}/>
+      </TodoList>
+    </>
+  );
+}
+
+function TodoHeader({ children }) {
+  return (
+    <header>
+      {children}
+    </header>
+  );
+}
+
+function TodoList({ children }) {
+  return (
+    <section className="todoList-container">
+      {children}
+    </section>
+  );
+}
+
+function TodoCounter() {
+  return <p style={tempStyle}>TodoCounter</p>;
+}
+
+function TodoSearch() {
+  return <p style={tempStyle}>TodoSearch</p>;
+}
+
+function TodoItem({ state }) {
+  return <p style={tempStyle}>TodoItem: {state}</p>;
+}
+/* function App() {
 
   return (
     <TodoProvider>
       <AppUI />
     </TodoProvider>
   );
-}
+} */
 
 export default App;
